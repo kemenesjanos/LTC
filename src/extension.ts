625dda38980d383840bb628ac915ea8d95d7b7ab
kemenesjanos <70114756+kemenesjanos@users.noreply.github.com>
2021-03-25 18:03:46 +0100

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,53 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
+import { SidebarProvider } from "./SidebarProvider";
+
+export function activate(context: vscode.ExtensionContext) {
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider("LTC-sidebar", sidebarProvider)
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("LTC.helloWorld", () => {
+      HelloWorldPanel.createOrShow(context.extensionUri);
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("LTC.refresh", async () => {
+      // HelloWorldPanel.kill();
+      // HelloWorldPanel.createOrShow(context.extensionUri);
+      await vscode.commands.executeCommand("workbench.action.closeSidebar");
+      await vscode.commands.executeCommand(
+        "workbench.view.extension.LTC-sidebar-view"
+      );
+      setTimeout(() => {
+        vscode.commands.executeCommand(
+          "workbench.action.webview.openDeveloperTools"
+        );
+      }, 500);
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("LTC.askQuestion", async () => {
+      const answer = await vscode.window.showInformationMessage(
+        "How was your day?",
+        "good",
+        "bad"
+      );
+
+      if (answer === "bad") {
+        vscode.window.showInformationMessage("Sorry to hear that");
+      } else {
+        console.log({ answer });
+      }
+    })
+  );
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    let todos = [];
+    let count = 0;
+</script>
+
+<input />
+
+<div>{count}</div>
+
+<button
+    on:dblclick={() => {
+        count += 8;
+    }}
+    on:click={() => {
+        count++;
+    }}
+>
+    increment
+</button>
+
+<style>
+    div {
+        color: blue;
+    }
+</style>
