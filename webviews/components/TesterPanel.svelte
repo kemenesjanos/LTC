00da050522dc3a4b7feb8894f6f831ae99fe6c4f
kemenesjanos <70114756+kemenesjanos@users.noreply.github.com>
2021-04-07 15:28:37 +0200

    safety before changes

---
+++ webviews/components/TesterPanel.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import Sidebar from "./Sidebar.svelte";
+    import DescriptionTab from "./tabs/DescriptionTab.svelte"
+    let test = "w";
+
+    //it is called when the svelte is ready
+    onMount(() => {
+
+        tsvscode.postMessage(
+            {
+                //Todo: implement this
+                command: 'init-view',
+                value: null
+            }
+        );
+    })
+
+    let jsonData = {
+        "DescriptionTabData":
+            {
+            "name": "Kezd",
+            "picture": "",
+            "shortDescription": "sk",
+            "description": "sf",
+            "example": "sfdv"
+            }
+}
+
+        window.addEventListener("message", (event) => {
+            const message = event.data;
+            switch (message.command) {
+                case "test-message":
+                    test = message.value
+                    return;
+                case 'add-message':
+
+                    jsonData = JSON.parse(message.value);
+
+				    return;
+                case 'update':
+                    const text = message.text;
+                    // Update our webview's content
+                    jsonData = JSON.parse(text);
+                    // Then persist state information.
+                    // This state is returned in the call to `vscode.getState` below when a webview is reloaded.
+                    tsvscode.setState({ text });
+
+                    return;
+            }
+        })
+    function pushDataUpdate() {
+		tsvscode.postMessage({
+			command: 'update',
+			value: jsonData
+		});
+	}
+
+    function handleMessage(event: { detail: { type: any; }; }) {
+	switch (event.detail.type) {
+		case "update":
+			pushDataUpdate();
+			break;
+		default:
+			break;
+	}
+}
+
+</script>
+
+
+<div>{test}</div>
+almaaaaaaaaaa
+<button on:click={() => {
+    tsvscode.postMessage({command: 'alert', value: 'Katalin meg Gáspár elmennek vásár!'});
+}}>Clk for error</button>
+
+<div/><div/><div/>
+
+<DescriptionTab bind:data={jsonData.DescriptionTabData} on:message={handleMessage} />
