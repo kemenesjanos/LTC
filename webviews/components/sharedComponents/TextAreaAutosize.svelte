e046f8628e3d17197a742ee2fadca30e4e092c58
Kemenes János <kemenes.janos1999@gmail.com>
2021-08-17 14:37:43 +0200

    Added max length to TextAreaAutosize

--- webviews/components/sharedComponents/TextAreaAutosize.svelte
+++ webviews/components/sharedComponents/TextAreaAutosize.svelte
@@ -1,7 +1,8 @@
 <script>
 	export let value = '';
 	export let minRows = 1;
-	export let maxRows = 100;
+	export let maxRows = 0;
+	export let maxLength = 0;
 	
 	$: minHeight = `${1 + minRows * 1.2}em`;
 	$: maxHeight = maxRows ? `${1 + maxRows * 1.2}em` : `auto`;
@@ -10,8 +11,8 @@
 <div class="container">
 	<pre
 		aria-hidden="true"
-		style="min-height: {minHeight}; max-height: {maxHeight}"
+		style="min-height: {minHeight}; max-height: {maxHeight};"
 	>{value + '\n'}</pre>
 
-	<textarea bind:value></textarea>	
+	<textarea maxLength={maxLength ? maxLength : null} bind:value></textarea>	
 </div>
